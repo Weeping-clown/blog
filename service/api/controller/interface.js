3cb75cd5934dddf4bc310663eb8545a72ee990c6
1178567243@qq.com <1178567243@qq.com>
2019-02-28 15:42:33 +0800

    小修改

--- service/api/controller/interface.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-const myrouter = require('./router.js')
-var router = Router()
-// index router
-router.get('/', myrouter.indexShow)
-router.get('/user', myrouter.indexShow)
-
-export default router
--- service/api/model/settings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    username: 'root',
-    password: 'root',
-    host: '127.0.0.1',
-    port: 3306,
-    name: 'blog'
-}
